a6feaa3e2d3e9946377bef3e5503c2765a4cf206
Tu Dang <tudang87@gmail.com>
2013-06-12 23:43:39 +0200

    Find RP plan violates Spread constraint with Vertical scale scenario.

--- src/main/java/evaluation/demo/Application.java
+++ src/main/java/evaluation/demo/Application.java
@@ -2,6 +2,7 @@ package evaluation.demo;
 
 import btrplace.model.Model;
 import btrplace.model.VM;
+import btrplace.model.constraint.Preserve;
 import btrplace.model.constraint.Running;
 import btrplace.model.constraint.SatConstraint;
 import btrplace.model.constraint.Spread;
@@ -18,37 +19,38 @@ import java.util.*;
  */
 public class Application {
      private static int index = 0;
-    ArrayList<VM> tier1;
-    ArrayList<VM> tier2;
-    ArrayList<VM> tier3;
-    ArrayList<ArrayList<VM>> tiers;
+    private ArrayList<VM> tier1;
+    private ArrayList<VM> tier2;
+    private ArrayList<VM> tier3;
+    private ArrayList<VM> vms;
+    private ArrayList<ArrayList<VM>> tiers;
+    private Collection<SatConstraint> checkConstraints;
 
     public Application(Model model) {
+        vms = new ArrayList<VM>();
         tier1 = new ArrayList<VM>();
         tier2 = new ArrayList<VM>();
         tier3 = new ArrayList<VM>();
         tiers = new ArrayList<ArrayList<VM>>();
-        tiers.addAll(Arrays.asList(tier1, tier2, tier3));
+        boolean b = tiers.addAll(Arrays.asList(tier1, tier2, tier3));
+        checkConstraints = new ArrayList<SatConstraint>();
 
         Random random = new Random(System.nanoTime() % 100000);
         for (int i = 0; i < 3; i++) {
             int n = random.nextInt(5) + 2;
-            ShareableResource sr = (ShareableResource) model.getView("ShareableResource.cpu");
-
             switch (i) {
                 case 0:
                     for (int j = 0; j < n; j++) {
                         VM vm = model.newVM(index++);
                         model.getMapping().addReadyVM(vm);
-                        sr.setConsumption(vm, 1);
                         tiers.get(i).add(vm);
+                        vms.add(vm);
                     }
                     break;
 
                 case 1:
                     for (int j = 0; j < n; j++) {
                         VM vm = model.newVM(index++);
-                        sr.setConsumption(vm, 4);
                         model.getMapping().addReadyVM(vm);
                         tiers.get(i).add(vm);
                     }
@@ -57,7 +59,6 @@ public class Application {
                 default:
                     for (int j = 0; j < n; j++) {
                         VM vm = model.newVM(index++);
-                        sr.setConsumption(vm, 2);
                         model.getMapping().addReadyVM(vm);
                         tiers.get(i).add(vm);
                     }
@@ -66,15 +67,20 @@ public class Application {
         }
     }
 
-    public Collection<SatConstraint> initConstraints() {
+    public Collection<SatConstraint> initConstraints(boolean cont) {
         Collection<SatConstraint> cts = new ArrayList<SatConstraint>();
         for (ArrayList<VM> t : tiers) {
-            Spread spread = new Spread(new HashSet<VM>(t));
+            Spread spread = new Spread(new HashSet<VM>(t), cont);
             Running run = new Running(t);
             cts.add(spread);
+            checkConstraints.add(spread);
             cts.add(run);
         }
         return cts;
+    }
+
+    public Collection<SatConstraint> getCheckConstraints() {
+        return checkConstraints;
     }
 
     @Override
@@ -82,5 +88,25 @@ public class Application {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("Tier1: %s\nTier2: %s\nTier3: %s\n", tier1, tier2, tier3));
         return sb.toString();
+    }
+
+    public Collection<SatConstraint> loadSpike(int percent) {
+        Collection<SatConstraint> constraints = new ArrayList<SatConstraint>();
+//        constraints.add(new Preserve(tier2, "cpu", 4));
+        int size = tier3.size() * percent / 100;
+        ArrayList<VM> tmp = new ArrayList<VM>();
+        for (int i = 0; i < size; i++) {
+            tmp.add(tier3.get(i));
+        }
+        constraints.add(new Preserve(tmp, "cpu", 4));
+        return constraints;
+    }
+
+    public Collection<VM> getAllVM() {
+        return vms;
+    }
+
+    public Collection<VM> getDatabaseVM() {
+        return tier3;
     }
 }
--- src/main/java/evaluation/demo/LoadPeak.java
+++ src/main/java/evaluation/demo/LoadPeak.java
@@ -1,13 +1,17 @@
 package evaluation.demo;
 
-import btrplace.model.*;
+import btrplace.model.DefaultModel;
+import btrplace.model.Model;
+import btrplace.model.Node;
 import btrplace.model.constraint.Among;
 import btrplace.model.constraint.SatConstraint;
+import btrplace.model.constraint.SingleRunningCapacity;
 import btrplace.model.view.ShareableResource;
 import btrplace.plan.ReconfigurationPlan;
 import btrplace.solver.SolverException;
 import btrplace.solver.choco.ChocoReconfigurationAlgorithm;
 import btrplace.solver.choco.DefaultChocoReconfigurationAlgorithm;
+import evaluation.generator.EvaluationTools;
 import org.testng.Assert;
 
 import java.util.ArrayList;
@@ -21,44 +25,101 @@ import java.util.Collection;
 public class LoadPeak {
     static Model model = new DefaultModel();
 
-    public static void main(String[] args) throws SolverException {
-        ShareableResource cpu = new ShareableResource("cpu", 32, 1);
+    public static void main(String[] args) {
+
+        ShareableResource cpu = new ShareableResource("cpu", 16, 1);
+        Collection<Application> appList = new ArrayList<Application>();
         model.attach(cpu);
 
         ArrayList<Collection<Node>> nss = new ArrayList<Collection<Node>>();
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < 8; j++) {
             ArrayList<Node> ns = new ArrayList<Node>();
-            for (int i = 0; i < 30; i++) {
+            for (int i = 0; i < 20; i++) {
                 Node node = model.newNode();
-                if(i%3 == 0) ns.add(node);
+                ns.add(node);
                 model.getMapping().addOnlineNode(node);
             }
             nss.add(ns);
         }
         Model clone = model.clone();
-        boolean cont;
-        int count = 0;
-        do {
-            Application app = new Application(model);
-            cont = runApp(app, nss);
-            Assert.assertNotEquals(model, clone);
-            System.out.println(String.format("Runned %d applications", ++count));
+
+        try {
+            boolean cont;
+            int count = 0;
+            int load;
+            do {
+                Application app = new Application(model);
+                appList.add(app);
+                // Set Spread, Among continuous restriction by the third parameter
+                cont = runApp(app, nss, false);
+
+                Assert.assertNotEquals(model, clone);
+//                System.out.println(String.format("Runned %d applications", ++count));
+                System.out.print(".");
+                load = EvaluationTools.currentLoad(model);
+            }
+            while (cont && load < 80);
+
+        } catch (SolverException e) {
+            System.err.println("Cannot host more applications");
         }
-        while (cont) ;
+        System.out.println("Current Load: " + EvaluationTools.currentLoad(model));
+        System.out.println(model.getMapping());
+        boolean satisfied = true;
+        do {
+            ReconfigurationPlan pl = reconfigureWhenLoadSpike(appList);
+
+            for (Application app : appList) {
+                for (SatConstraint s : app.getCheckConstraints()) {
+                    boolean continuous = s.isContinuous();
+                    if (!continuous) s.setContinuous(true);
+                    if (!s.isSatisfied(pl)) {
+                        satisfied = false;
+                        System.out.println("Does not satisfy " + s);
+                        System.exit(-1);
+                    }
+                    s.setContinuous(continuous);
+                }
+            }
+        } while (satisfied);
+        System.out.println("Continuous satisfaction");
+
     }
 
-    public static boolean runApp(Application app, Collection<Collection<Node>> nodeSets) throws SolverException {
+    public static boolean runApp(Application app, Collection<Collection<Node>> nodeSets, boolean cont) throws SolverException {
         ChocoReconfigurationAlgorithm cra = new DefaultChocoReconfigurationAlgorithm();
         cra.setTimeLimit(10);
-        Collection<SatConstraint> cstrs = app.initConstraints();
-        Among among = new Among(app.tier3, nodeSets);
+        Collection<SatConstraint> cstrs = app.initConstraints(cont);
+        Among among = new Among(app.getDatabaseVM(), nodeSets, cont);
+        for (Collection<Node> nodes : nodeSets) {
+            SingleRunningCapacity src = new SingleRunningCapacity(nodes, 6);
+            cstrs.add(src);
+        }
         cstrs.add(among);
-        ReconfigurationPlan plan =  cra.solve(model, cstrs);;
-        if(plan!=null) {
+        ReconfigurationPlan plan = cra.solve(model, cstrs);
+
+        if (plan != null) {
             model = plan.getResult();
             return true;
         }
         return false;
+    }
+
+    public static ReconfigurationPlan reconfigureWhenLoadSpike(Collection<Application> apps) {
+        ChocoReconfigurationAlgorithm cra = new DefaultChocoReconfigurationAlgorithm();
+        Collection<SatConstraint> cstrs = new ArrayList<SatConstraint>();
+        for (Application a : apps) {
+            cstrs.addAll(a.loadSpike(30));
+            cstrs.addAll(a.getCheckConstraints());
+        }
+        try {
+            ReconfigurationPlan plan = cra.solve(model, cstrs);
+            System.out.println(cra.getSolvingStatistics());
+            return plan;
+        } catch (SolverException e) {
+            System.err.println("No problem");
+            return null;
+        }
     }
 
 }
